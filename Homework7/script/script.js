e3fe07c78da3cfd2b3305a9ff315ebb5f3388ccc
aidanaimankulova <164762642+aidanaimankulova@users.noreply.github.com>
2024-05-26 15:18:26 +0200

    Add files via upload

---
+++ Homework7/script/script.js
@@ -0,0 +1,38 @@
+// work1
+// let number = +prompt("Enter the number");
+
+// if(number > 0){
+//     console.log(number*3);
+// }
+//    else{
+//     console.log(number*2);
+//    } 
+
+
+// work2
+// let students = +prompt("enter the numbers of stundent");
+// let chairs = +prompt("enter the numbers of chairs")
+
+// if(students <= chairs){
+//     console.log("стульев хватает");
+// } else{
+//     console.log("стульев не хватает");
+// }
+
+
+// work3
+let names = ['John', 'Bob', 'Tom', 'Nick', 'Sam', 'Alex', 'Jack']
+console.log(names);
+console.log(names[5]);
+
+names.shift();
+console.log(names);
+
+names.push("London");
+console.log(names);
+
+names[4]="Den";
+console.log(names);
+
+names.pop();
+console.log(names);
